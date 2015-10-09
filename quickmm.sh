c1a5e511cae7028d8ff8ee291119c3b56180b8c6
Evaldas Taroza <evaldas@taroza.lt>
2015-10-09 22:09:16 +0200

    Update quickmm.sh
    exclude LICENSE.txt

--- quickmm.sh
+++ quickmm.sh
@@ -4,7 +4,7 @@ if ( [ -n "$1" ])
 then
   cd $1
   echo "Creating modmam file for each file"
-  for i in `find . -type f | grep -v \.git | grep -v "^.$" | grep -v "modman" | grep -v "composer.json" | grep -v "package.xml" | grep -v "README.md" | grep -v \.DS_Store | sed 's/\.\///'`; do echo ${i} ${i}; done > modman
+  for i in `find . -type f | grep -v \.git | grep -v "^.$" | grep -v "modman" | grep -v "composer.json" | grep -v "package.xml" | grep -v "README.md" | grep -v "LICENSE.txt" | grep -v \.DS_Store | sed 's/\.\///'`; do echo ${i} ${i}; done > modman
 
 fi
 
